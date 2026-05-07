411ab19dbdc0d663fa949a061a14a0ac1bead691
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-05-07 19:59:33 +0200

    Ajout du changement de langue FR/EN

--- script.js
+++ script.js
@@ -15,12 +15,151 @@ const CurrentMonth = document.querySelector("nav li:first-child");
 //const tweaks = document.querySelector("nav li:second-child");
 //const AboutUs = document.querySelector("nav li:third-child");
 
+const translations = {
+    fr:{
+        APropos: "À propos de nous",
+        budgetMois: "BUDGET DU MOIS",
+        editMois: "Editer",
+        depenseMois:"DÉPENSÉ CE MOIS",
+        moyDepense:"Moyenne des dépenses:",
+        solde: "SOLDE",
+
+        ajoutDepense:"Ajouter une dépense",
+        description: "Description",
+        montant: "Montant",
+        category: "Catégorie",
+        boutonAjout: "+ Ajouter",
+
+        transaction:"Transactions",
+        tout: "Tout",
+        nourriture: "Nourriture",
+        transport: "Transport",
+        logement: "Logement",
+        sante: "Santé",
+        sub:"Abonnements",
+        shopping:"Shooping",
+        rest:"Restaurants",
+        voyage:"Voyages",
+        hobby:"Hobbies",
+        autres: "Autres",
+        touSupprimer:"Tout supprimer",
+
+        titre:"Mon tracker de dépenses",
+
+        darkModetransl:"Mode sombre",
+        mode: "Mode:",
+        langue:"Langue:",
+        devise:"Devise:"
+    },
+
+    en:{
+        APropos: "About us",
+        budgetMois: "MONTH BUDGET",
+        editMois: "Edit",
+        depenseMois:"SPENT THIS MONTH",
+        moyDepense:"Mean of expenses",
+        solde: "BALANCE",
+
+        ajoutDepense:"Add an expense",
+        description: "Description",
+        montant: "Amount",
+        category: "Category",
+        boutonAjout: "+ Add",
+
+        transaction:"Transactions",
+        tout: "All",
+        nourriture: "Food",
+        transport: "Transport",
+        logement: "Housing",
+        sante: "Health",
+        sub:"Subscriptions",
+        shopping:"Shooping",
+        rest:"Restaurants",
+        voyage:"Trips",
+        hobby:"Hobbies",
+        autres: "Others",
+        touSupprimer:"Delete all",
+
+        titre:"My Expense Tracker",
+
+        darkModetransl:"Dark Mode",
+        mode: "Mode:",
+        langue:"Language:",
+        devise:"Currency:"
+    }
+}
+
 
 let transactions = [];
 
 function saveToLocalStorage(){
     localStorage.setItem("Trans", JSON.stringify(transactions));
 };
+
+function setLanguage(lang){
+    const traduction = translations[lang];
+
+    document.querySelector("h1").textContent = traduction.titre;
+    document.querySelector("nav li:nth-child(3)").textContent = traduction.APropos;
+    document.querySelector(".budget-left h3").textContent = traduction.budgetMois;
+    document.querySelector(".before-jauge button").textContent = traduction.editMois;
+    document.querySelector(".expense-container h3").textContent = traduction.depenseMois;
+    //document.querySelector(".expense-mean").textContent = traduction.moyDepense;
+    document.querySelector(".balance-container h3").textContent = traduction.solde;
+    document.querySelector(".add-expense h2").textContent = traduction.ajoutDepense;
+    document.querySelector("#expense-name").placeholder = traduction.description;
+    document.querySelector("#expense-amount").placeholder = traduction.montant;
+    document.querySelector("#category-id option[value=''] ").textContent = traduction.category;
+    document.querySelector("#category-id option[value='food']").textContent = traduction.nourriture;
+    document.querySelector("#category-id option[value='transport']").textContent = traduction.transport;
+    document.querySelector("#category-id option[value='housing']").textContent = traduction.logement;
+    document.querySelector("#category-id option[value='health']").textContent = traduction.sante;
+    document.querySelector("#category-id option[value='sub']").textContent = traduction.sub;
+    document.querySelector("#category-id option[value='shopping']").textContent = traduction.shopping;
+    document.querySelector("#category-id option[value='rest']").textContent = traduction.rest;
+    document.querySelector("#category-id option[value='trips']").textContent = traduction.voyage;
+    document.querySelector("#category-id option[value='hobbies']").textContent = traduction.hobby;
+    document.querySelector("#category-id option[value='others']").textContent = traduction.autres;
+    document.querySelector(".add-button").textContent = traduction.boutonAjout;
+    document.querySelector(".transactions-header h2").textContent = traduction.transaction;
+    document.querySelector(".delete-all").textContent = traduction.touSupprimer;
+    document.querySelector(".category-class:first-child .category-text").textContent = traduction.tout;
+    document.querySelector(".category-class:nth-child(2) .category-text").textContent = traduction.nourriture;
+    document.querySelector(".category-class:nth-child(3) .category-text").textContent = traduction.transport;
+    document.querySelector(".category-class:nth-child(4) .category-text").textContent = traduction.logement;
+    document.querySelector(".category-class:nth-child(5) .category-text").textContent = traduction.sante;
+    document.querySelector(".category-class:nth-child(6) .category-text").textContent = traduction.sub;
+    document.querySelector(".category-class:nth-child(7) .category-text").textContent = traduction.shopping;
+    document.querySelector(".category-class:nth-child(8) .category-text").textContent = traduction.rest;
+    document.querySelector(".category-class:nth-child(9) .category-text").textContent = traduction.voyage;
+    document.querySelector(".category-class:nth-child(10) .category-text").textContent = traduction.hobby
+    document.querySelector(".category-class:nth-child(11) .category-text").textContent = traduction.autres;
+    document.querySelector(".mode label").textContent = traduction.darkModetransl;
+    document.querySelector(".mode p").textContent = traduction.mode;
+    document.querySelector(".language p").textContent = traduction.langue;
+    document.querySelector(".devise p").textContent = traduction.devise;
+
+    if (lang === "fr"){
+        moisActuel = `${new Date().toLocaleString('fr-FR', {month: 'long'}).charAt(0).toUpperCase() +
+        new Date().toLocaleString('fr-FR', {month: 'long'}).slice(1)} ${new Date().getFullYear()}`;
+    }
+    else if(lang === "en"){
+        moisActuel = `${new Date().toLocaleString('en-EN', {month: 'long'}).charAt(0).toUpperCase() +
+        new Date().toLocaleString('en-EN', {month: 'long'}).slice(1)} ${new Date().getFullYear()}`;
+    };
+    CurrentMonth.textContent = moisActuel;
+
+}
+
+langselected = document.querySelectorAll(".language button");
+langselected.forEach(i => i.addEventListener("click", () => {
+    langselected.forEach(btn => btn.classList.remove("active"));
+    i.classList.add("active");
+    languageValue = i.dataset.lang;
+    setLanguage(languageValue);
+    localStorage.setItem("langStored", languageValue);
+    updateExpense();
+}));
 
 
 
@@ -97,6 +236,8 @@ function editBudget(){
 
 };
 
+let languageValue = "en"; // langue par défaut
+
 function updateExpense(){
     let total_expense = transactions.reduce(  (sum, currentValue)=> sum + currentValue.expenseAmount ,0   );
 
@@ -113,7 +254,7 @@ function updateExpense(){
     expenseContainer.textContent = total_expense;
     transactionTotal.textContent = transactions.length;
 
-    expenseMean.textContent = `Mean of expenses : ${mean_expense}` ;
+    expenseMean.textContent = `${translations[languageValue].moyDepense} : ${mean_expense}` ;
 
 
     CategoryClass.forEach(
@@ -180,18 +321,12 @@ CurrentMonthvar ="";
 document.addEventListener("DOMContentLoaded", ()=>{
     closeModaleFunc();
 
-    let modeValue = localStorage.getItem("mode");
-    if (modeValue === "true"){
-
-        document.body.classList.add("dark");
-        darkMode.checked = true;
-
-    }else{
-
-    };
+    languageValue = localStorage.getItem("langStored") || "en" ;
+    setLanguage(languageValue);
+    document.querySelector(`.language button[data-lang="${languageValue}"]`).classList.add("active");
 
 
-    let moisActuel = `${new Date().toLocaleString('en-US', {month: 'long'})} ${new Date().getFullYear()}`;
+
     let moisSauvegarde = localStorage.getItem("moisSauvegarde");
     if (moisActuel !== moisSauvegarde){
         transactions = [];
@@ -207,6 +342,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     }
     CurrentMonth.textContent = moisSauvegarde;
+
+
+    let modeValue = localStorage.getItem("mode");
+    if (modeValue === "true"){
+
+        document.body.classList.add("dark");
+        darkMode.checked = true;
+
+    }else{
+
+    };
+
+
+
 
 
     let currentTrans = localStorage.getItem("Trans");
@@ -226,6 +375,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
 
     budgetContainer.textContent = Budgets;
+
 
     updateExpense();
 
